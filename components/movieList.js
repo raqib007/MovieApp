00d5ccbe6dcaf6ef7f664bec884ff30468289045
Raqib Hasan <crytekredhat@gmail.com>
2020-08-31 13:31:29 +0600

    second commit

--- components/movieList.js
+++ components/movieList.js
@@ -1,4 +1,12 @@
 class MovieList extends React.Component {
+
+  shorten = (text,maxLength) =>{
+    if(text && text.length > maxLength){
+      return text.substr(0,maxLength)+'...';
+    }
+    return text; 
+  }
+
   renderMovies = (movies) => {
     return movies.map((movie) => (
       <div className="col-lg-4 col-md-6 mb-4" key={movie.id}>
@@ -10,7 +18,7 @@ class MovieList extends React.Component {
             <h4 className="card-title">
               <a href="#">{movie.name}</a>
             </h4>
-            <p className="card-text">{movie.description}</p>
+            <p className="card-text">{this.shorten(movie.description,200)}</p>
           </div>
           <div className="card-footer">
             <small className="text-muted">{movie.rating}</small>
